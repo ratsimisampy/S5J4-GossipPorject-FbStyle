d20c7a6d335499d9def49f2d7b3b9e2c3a05d884
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-11-02 01:03:26 +0100

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,4 @@ Rails.application.routes.draw do
 
   resources :gossips
   
-  get '/new', to: 'gossip#new'
-  post '/new', to: 'gossip#create'
-
-  get '/gossip/:id', to: 'gossip#show' 
-
-  get '/gossip/edit/:id', to: 'gossip#edit'
-  post '/gossip/edit/:id', to: 'gossip#update'
-  delete '/gossip/:id', to: 'gossip#destroy'
-
-  get '/gossip', to: 'gossip#index'
-
-  get '/gossip/:gossip_id', to: 'comments#index'
-
-  get '/gossip/:gossip_id', to: 'comments#new'
-  post '/gossip/:gossip_id', to: 'comments#create'
-
-  get '/comments/edit/:id', to: 'comments#edit'
-  post '/comments/edit/:id', to: 'comments#update'
-  delete '/comments/edit/:id', to: 'comments#destroy'
-  
 end
